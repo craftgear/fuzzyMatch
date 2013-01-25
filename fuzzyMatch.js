3878599731c2a5f5bbb5b71e48938db97a89a7a9
Watanabe, Shunsuke <watanabe@craftgear.net>
2013-01-25 09:53:42 +0900

    add ignore diffrence regex character set

--- fuzzyMatch.js
+++ fuzzyMatch.js
@@ -1,44 +1,72 @@
 var fuzzyMatch = {
-  fuzzyTailMatch: function (search, target, minimum_char_length){
+  fuzzyTailMatch: function(search, target, minimum_char_length, ignore_difference_regexp_char_set) {
     if (!minimum_char_length) {
       minimum_char_length = 3;
+    }
+    if (search < minimum_char_length) {
+      return false;
+    }
+    if (ignore_difference_regexp_char_set === null) {
+      ignore_difference_regexp_char_set = '[\\s_]'; //default we consider space and underscore as the same thing
     }
     var len = search.length;
     var jump = parseInt(len / 2, 10);
     var start_index_in_target = null;
     var matched_length_of_search = 0;
 
-    while(true){
+    while (true) {
+
+      if (target.indexOf(search) > -1) {
+        return {
+          matched_length_of_search: search.length,
+          matched: search,
+          start_index_in_target: target.indexOf(search)
+        };
+      }
+
       var is_matched = target.indexOf(search.substr(0, matched_length_of_search + jump));
-      if(is_matched > -1){
+      if (is_matched > -1) {
         matched_length_of_search = matched_length_of_search + jump;
         start_index_in_target = is_matched;
       }
       jump = parseInt(jump / 2, 10);
 
-      if(jump === 0){
-        if(matched_length_of_search === 0 || matched_length_of_search < minimum_char_length){
+      if (jump === 0) {
+        if (matched_length_of_search === 0 || matched_length_of_search < minimum_char_length) {
           return false;
         }
-        if(target.indexOf(search.substr(0, matched_length_of_search)) > -1 && target.indexOf(search.substr(0, matched_length_of_search + 1)) === -1){
-          return {matched_length_of_search: matched_length_of_search, matched: search.substr(0, matched_length_of_search),start_index_in_target: start_index_in_target };
+        if (target.indexOf(search.substr(0, matched_length_of_search)) > -1 && target.indexOf(search.substr(0, matched_length_of_search + 1)) === -1) {
+
+          //now we go crazy!
+          var regexp = new RegExp(search.substr(0, matched_length_of_search) + ignore_difference_regexp_char_set);
+          if (search.substr(matched_length_of_search, 1).match(ignore_difference_regexp_char_set) !== null && target.match(regexp) !== null) {
+            search = target.match(regexp)[0] + search.substr(matched_length_of_search + 1);
+            jump = parseInt((search.length - matched_length_of_search) / 2, 10);
+            continue;
+          } else {
+            return {
+              matched_length_of_search: matched_length_of_search,
+              matched: search.substr(0, matched_length_of_search),
+              start_index_in_target: start_index_in_target
+            };
+          }
+        } else {
+          jump = 1;
         }
-        jump = 1;
       }
     }
   },
 
-  fuzzyTailReplace: function (search, target, replace){
+  fuzzyTailReplace: function(search, target, replace) {
     var result = this.fuzzyTailMatch(search, target);
-    if(result === false){
+    if (result === false) {
       return target;
-    }
-    else {
+    } else {
       return target.replace(result.matched, replace);
     }
   }
 };
 
-if(typeof exports === 'object'){
+if (typeof exports === 'object') {
   module.exports = fuzzyMatch;
 }
--- tests/fuzzyMatchTest.js
+++ tests/fuzzyMatchTest.js
@@ -1,27 +1,35 @@
-if(typeof exports === 'object'){
+if (typeof exports === 'object') {
   var expect = require('expect.js');
   var fuzzyMatch = require('../fuzzyMatch.js');
 }
 
-describe('fuzzyMatch', function(){
-  describe('fuzzyMatch.fuzzyTailMatch', function () {
-    it('should match aaabbb with dddaaaccc only by aaa', function(done){
+describe('fuzzyMatch', function() {
+  describe('fuzzyMatch.fuzzyTailMatch', function() {
+    it('should match aaabbb with dddaaaccc only by aaa', function(done) {
       var search = 'aaabbb';
       var target = 'aaaccc';
       var result = fuzzyMatch.fuzzyTailMatch(search, target);
-      expect(result).eql({matched_length_of_search:3, matched:'aaa', start_index_in_target:0});
+      expect(result).eql({
+        matched_length_of_search: 3,
+        matched: 'aaa',
+        start_index_in_target: 0
+      });
       done();
     });
 
-    it('should match あいうえお:かきくけこ with かきくけこあいうえおさしすせそ, only by あいうえお', function(done){
+    it('should match あいうえお:かきくけこ with かきくけこあいうえおさしすせそ, only by あいうえお', function(done) {
       var search = 'あいうえお:かきくけこ';
       var target = 'かきくけこあいうえおさしすせそ';
       var result = fuzzyMatch.fuzzyTailMatch(search, target);
-      expect(result).eql({matched_length_of_search:5, matched:'あいうえお', start_index_in_target:5});
+      expect(result).eql({
+        matched_length_of_search: 5,
+        matched: 'あいうえお',
+        start_index_in_target: 5
+      });
       done();
     });
 
-    it('should not match aaa with bbb', function(done){
+    it('should not match aaa with bbb', function(done) {
       var search = 'aaa';
       var target = 'bbb';
       var result = fuzzyMatch.fuzzyTailMatch(target, search);
@@ -29,17 +37,41 @@ describe('fuzzyMatch', function(){
       done();
     });
 
-    it('should not match less than minimum_char_length', function (done) {
+    it('should not match less than minimum_char_length', function(done) {
       var search = 'Aaaabbbbcc';
       var target = 'Abcdefghij';
       var result = fuzzyMatch.fuzzyTailMatch(search, target, 3);
       expect(result).to.be(false);
       done();
     });
+
+    it('should ignore diffrence between space and underscore', function(done) {
+      var search = 'Aaaabbbbcc_dddeeefff';
+      var target = 'Aaaabbbbcc dddeeefff';
+      var result = fuzzyMatch.fuzzyTailMatch(search, target);
+      expect(result).eql({
+        matched_length_of_search: 20,
+        matched: 'Aaaabbbbcc dddeeefff',
+        start_index_in_target: 0
+      });
+      done();
+    });
+
+    it('should not ignore diffrence between characters other than space and underscore', function(done) {
+      var search = 'Aaaabbbbcc_dddeeefff';
+      var target = 'Aaaabbbbccあdddeeefff';
+      var result = fuzzyMatch.fuzzyTailMatch(search, target);
+      expect(result).eql({
+        matched_length_of_search: 10,
+        matched: 'Aaaabbbbcc',
+        start_index_in_target: 0
+      });
+      done();
+    });
   });
 
-  describe('fuzzyMatch.fuzzyTailReplace', function () {
-    it('should replace aaabbb with aaaccc and ddd return dddbbb', function (done) {
+  describe('fuzzyMatch.fuzzyTailReplace', function() {
+    it('should replace aaabbb with aaaccc and ddd return dddbbb', function(done) {
       var result = fuzzyMatch.fuzzyTailReplace('aaaccc', 'cccaaabbb', 'ddd');
       expect(result).eql('cccdddbbb');
       done();
